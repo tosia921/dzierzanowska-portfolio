5df6d9b112ea74cc879f86be3b7a9a59ba480421
Tomasz Posiadala <tomaszposiadala@gmail.com>
2021-07-27 20:15:14 +0100

    added tailwind.css and tailwind.congif.js files, added custom fonts to tailwind config and css file.

--- nextjs-frontend/tailwind.config.js
+++ nextjs-frontend/tailwind.config.js
@@ -1,9 +1,16 @@
+const defaultTheme = require('tailwindcss/defaultTheme');
+
 module.exports = {
   mode: 'jit',
   purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        'playlist': ["'Playlist Script'", ...defaultTheme.fontFamily.mono],
+        'mali': ["'Mali'", ...defaultTheme.fontFamily.mono],
+      }
+    },
   },
   variants: {
     extend: {},
